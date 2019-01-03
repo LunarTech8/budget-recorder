dbb790ad958a7bc79c08221a0e9a004c0d8bd5e9
LunarTech8 <lunartech8@gmail.com>
2019-01-03 16:46:07 +0100

    Initial commit

---
+++ src/main/java/com/romanbrunner/apps/budgetrecorder/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.romanbrunner.apps.budgetrecorder;
+
+// import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World 1");
+		System.out.println("Hello World 2");
+		// SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
