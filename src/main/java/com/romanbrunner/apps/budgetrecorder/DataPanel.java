d00f34c853d56530e93f65f5bba526a28e5a0d1f
LunarTech8 <lunartech8@gmail.com>
2019-04-09 21:03:59 +0200

    Work on DataPanel
    - Started implementation to make the header row stick

--- src/main/java/com/romanbrunner/apps/budgetrecorder/DataPanel.java
+++ src/main/java/com/romanbrunner/apps/budgetrecorder/DataPanel.java
@@ -7,6 +7,7 @@ import java.awt.Dimension;
 
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.JLabel;
 
 
@@ -20,7 +21,7 @@ class DataPanel extends JPanel
 	private static final int DATA_FIELD_WIDTH = 120;
 	private static final int DATA_FIELD_HEIGHT = 40;
 	private static final int HEADER_HEIGHT = 50;
-	private static final int DATA_PANEL_HIGHT = 400;
+	private static final int DATA_PANEL_HIGHT = 0;//400;
 	private static final String HEADER_TEXT = "<NAME>:";
 	private static final String HEADER_TOOLTIP = "Shows the values for <NAME> in the fields below.";
 	private static final String DATA_FIELD_TOOLTIP = "Data field value for <NAME>.";
@@ -32,52 +33,63 @@ class DataPanel extends JPanel
 
 	public DataPanel()
 	{
-		super(new BorderLayout());
+		super(new GridBagLayout());
 		try
 		{
-			var dataRowsPanel = new JPanel(new GridBagLayout());
-			GridBagConstraints c = new GridBagConstraints();
+			GridBagConstraints mainConstrains = new GridBagConstraints();
 			// Define default constrains:
-			c.fill = GridBagConstraints.BOTH;
-			c.weightx = 0.5;
-			c.weighty = 0.5;
+			mainConstrains.weightx = 0.5;
+			mainConstrains.weighty = 0.5;
 
+			mainConstrains.fill = GridBagConstraints.HORIZONTAL;
+			mainConstrains.weighty = 0;
+			mainConstrains.gridx = 0;
+			mainConstrains.gridy = 0;
 			// Create header labels:
-			c.gridx = 0;
-			c.gridy = 0;
 			for (var dataRowType : DataEntry.DataRowType.values())
 			{
 				var name = dataRowType.toString();
-				var header = new JLabel(HEADER_TEXT.replace("<NAME>", name), JLabel.CENTER);
+				var header = new JLabel(HEADER_TEXT.replace("<NAME>", name), JLabel.LEFT);
 				header.setToolTipText(HEADER_TOOLTIP.replace("<NAME>", name));
 				header.setPreferredSize(new Dimension(DATA_FIELD_WIDTH, HEADER_HEIGHT));
-				dataRowsPanel.add(header, c);
+				add(header, mainConstrains);
 
-				c.gridx++;
+				mainConstrains.gridx++;
 			}
+
+			mainConstrains.fill = GridBagConstraints.BOTH;
+			mainConstrains.weighty = 1;
+			mainConstrains.gridx = 0;
+			mainConstrains.gridy = 1;
+			mainConstrains.gridwidth = DataEntry.DATA_ROW_TYPE_COUNT;
 			// Create data field labels:
-			c.gridy = 1;
+			var dataRowsPanel = new JPanel(new GridBagLayout());
+			GridBagConstraints dataRowsConstrains = new GridBagConstraints();
+			dataRowsConstrains.fill = GridBagConstraints.BOTH;
+			dataRowsConstrains.weightx = 0.5;
+			dataRowsConstrains.weighty = 0.5;
+			dataRowsConstrains.gridy = 0;
 			for (var dataEntry : MainFrame.getDataEntries())
 			{
-				c.gridx = 0;
+				dataRowsConstrains.gridx = 0;
 				for (var dataRowType : DataEntry.DataRowType.values())
 				{
 					var name = dataRowType.toString();
-					var label = new JLabel(dataEntry.getDataRow(dataRowType), JLabel.CENTER);
+					var label = new JLabel(dataEntry.getDataRow(dataRowType), JLabel.LEFT);
 					label.setToolTipText(DATA_FIELD_TOOLTIP.replace("<NAME>", name));
 					label.setPreferredSize(new Dimension(DATA_FIELD_WIDTH, DATA_FIELD_HEIGHT));
-					dataRowsPanel.add(label, c);
+					dataRowsPanel.add(label, dataRowsConstrains);
 
-					c.gridx++;
+					dataRowsConstrains.gridx++;
 				}
 
-				c.gridy++;
+				dataRowsConstrains.gridy++;
 			}
-
 			// Put the data rows panel in a scroll pane:
-			var scroller = new JScrollPane(dataRowsPanel);
-			scroller.setPreferredSize(new Dimension(DATA_FIELD_WIDTH * (DataEntry.DATA_ROW_TYPE_COUNT + 1), DATA_PANEL_HIGHT));  // Add one entry to width to avoid a width scrollbar
-			add(scroller, BorderLayout.CENTER);
+			var dataRowsScroller = new JScrollPane(dataRowsPanel);
+			dataRowsScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+			dataRowsScroller.setPreferredSize(new Dimension(DATA_FIELD_WIDTH * (DataEntry.DATA_ROW_TYPE_COUNT + 1), DATA_PANEL_HIGHT));  // Add one entry to width to avoid a width scrollbar
+			add(dataRowsScroller, mainConstrains);
 		}
 		catch (Exception exception)
 		{
